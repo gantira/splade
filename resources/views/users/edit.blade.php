4164cfbf40a26797fbbd6c4527f95640b2a403e4
Angga Gantira <angga.gantira@gmail.com>
2022-12-24 23:59:58 +0700

    form

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,14 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <x-splade-defer url="http://api.quotable.io/random">
-                        <p v-text="response.content"></p>
-                        <p v-if="processing">Proccessing..</p>
-                        <button class="" @click="reload">Reload</button>
-                    </x-splade-defer>
+                    <x-splade-form confirm="Update nih?" method="patch" :default="$user" :action="route('user.update', $user)">
+                        <x-splade-input type="text" name="name" />
+                        <x-splade-submit>Update</x-splade-submit>
+                    </x-splade-form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,6 +34,8 @@ Route::middleware('splade')->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::get('/user/{user}', [UserController::class, 'edit'])->name('user.edit');
+        Route::patch('/user/{user}', [UserController::class, 'update'])->name('user.update');
     });
 
     require __DIR__.'/auth.php';
